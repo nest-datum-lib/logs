bf831b6b3d8081e35888b2063285eec2bd861935
ihor <ihor.bielchenko@gmail.com>
2022-12-09 02:37:45 +0100

    test

--- nest-datum/balancer/src/balancer.module.ts
+++ nest-datum/balancer/src/balancer.module.ts
@@ -4,6 +4,8 @@ import { redisConfig } from 'config/redis';
 import { BalancerRepository } from './balancer.repository';
 import { BalancerService } from './balancer.service';
 
+console.log('redisConfig 2222', redisConfig, process['REDIS_CACHE'], process['REDIS_BALANCER']);
+
 @Module({
 	imports: [
 		RedisModule.forRoot(redisConfig),
