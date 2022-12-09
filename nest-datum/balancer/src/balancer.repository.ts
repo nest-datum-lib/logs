565248bc7b1ef36c294088f3812bfcaa2304341b
ihor <ihor.bielchenko@gmail.com>
2022-12-09 02:52:52 +0100

    test

--- nest-datum/balancer/src/balancer.repository.ts
+++ nest-datum/balancer/src/balancer.repository.ts
@@ -41,39 +41,41 @@ export class BalancerRepository extends RedisRepository {
 	async selectLessLoaded(payload: object) {
 		let output;
 
-		if (payload['id']
-			&& typeof payload['id'] === 'string') {
-			console.log('selectLessLoaded 1', this.balancerRepository);
+		try {
+			if (payload['id']
+				&& typeof payload['id'] === 'string') {
+				output = await this.balancerRepository.hmget(`${process['PROJECT_ID']}|${BalancerRepository.EntityName}|id`, payload['id']);
+			}
+			else {
+				let lessLoaderId,
+					lessLoader;
+				const allNamesData = await this.balancerRepository.hgetall(`${process['PROJECT_ID']}|${BalancerRepository.EntityName}|name`);
 
-			output = await this.balancerRepository.hmget(`${process['PROJECT_ID']}|${BalancerRepository.EntityName}|id`, payload['id']);
-		}
-		else {
-			let lessLoaderId,
-				lessLoader;
-			console.log('selectLessLoaded 2', this.balancerRepository);
-			const allNamesData = await this.balancerRepository.hgetall(`${process['PROJECT_ID']}|${BalancerRepository.EntityName}|name`);
+				for (payload['id'] in allNamesData) {
+					if (payload['name']
+						&& typeof payload['name'] === 'string'
+						&& payload['name'] === allNamesData[payload['id']]) {
+						const indicator = Number(await this.balancerRepository.hmget(`${process['PROJECT_ID']}|${BalancerRepository.EntityName}|serviceResponsLoadingIndicator`, payload['id']));
 
-			for (payload['id'] in allNamesData) {
-				if (payload['name']
-					&& typeof payload['name'] === 'string'
-					&& payload['name'] === allNamesData[payload['id']]) {
-					const indicator = Number(await this.balancerRepository.hmget(`${process['PROJECT_ID']}|${BalancerRepository.EntityName}|serviceResponsLoadingIndicator`, payload['id']));
-
-					if (indicator === 0) {
-						return await this.findOne(payload['id']);
-					}
-					if (lessLoader > indicator
-						|| typeof lessLoader === 'undefined') {
-						lessLoader = indicator;
-						lessLoaderId = payload['id'];
+						if (indicator === 0) {
+							return await this.findOne(payload['id']);
+						}
+						if (lessLoader > indicator
+							|| typeof lessLoader === 'undefined') {
+							lessLoader = indicator;
+							lessLoaderId = payload['id'];
+						}
 					}
 				}
+				if (!lessLoaderId) {
+					return null;
+				}
+				output = await this.findOne(lessLoaderId);
 			}
-			if (!lessLoaderId) {
-				return null;
-			}
-			output = await this.findOne(lessLoaderId);
+			return output;
 		}
-		return output;
+		catch (err) {
+			console.error(err);
+		}
 	}
 }
--- nest-datum/redis/src/redis.repository.ts
+++ nest-datum/redis/src/redis.repository.ts
@@ -11,8 +11,6 @@ export class RedisRepository {
 	}
 
 	async scan(match: string, count: number = 64) {
-		console.log('scan', this.redisRepository);
-
 		return await (new Promise(async (resolve, reject) => {
 			let scanStream,
 				output = []; 
@@ -44,8 +42,6 @@ export class RedisRepository {
 	}
 
 	async find(payload?: object): Promise<any> {
-		console.log('find', this.redisRepository);
-
 		if (payload
 			&& typeof payload === 'object'
 			&& payload['where']
@@ -136,8 +132,6 @@ export class RedisRepository {
 	}
 
 	async findOne(id: string, select?: Array<any>): Promise<any> {
-		console.log('findOne', this.redisRepository);
-
 		let i = 0,
 			output = {};
 		const schema = ((select || []).length > 0)
@@ -161,8 +155,6 @@ export class RedisRepository {
 	}
 
 	async delete(id: string): Promise<any> {
-		console.log('delete', this.redisRepository);
-
 		let i = 0;
 
 		while (i < this.schema.length) {
@@ -180,8 +172,6 @@ export class RedisRepository {
 	}
 
 	async create(payload: object): Promise<any> {
-		console.log('create', this.redisRepository);
-
 		const id = (typeof payload['id'] === 'string'
 			&& payload['id'].length >= 1
 			&& payload['id'].length <= 64)
@@ -206,8 +196,6 @@ export class RedisRepository {
 	}
 
 	async update(id: string, payload: object): Promise<any> {
-		console.log('update', this.redisRepository);
-		
 		let key;
 
 		for (key in payload) {
